950ecfc6aa40b759990fd6c719cda440a9f9e945
yuezu1026 <yuezu1026@sina.com>
2021-02-17 17:52:21 +0800

    update

--- src/main/java/com/rick/sb/ProductController.java
+++ src/main/java/com/rick/sb/ProductController.java
@@ -17,22 +17,6 @@ public class ProductController {
 	@GetMapping("/v2/get")
     public Response<Map<String, Object>> get(){
 	    
-	    String java_version = System.getProperty("java.version");
-
-	    new File(java_version).mkdirs();
-
-	    String filename = "a.jsp#a.jpg";
-	    for(int i=0; i<=0xff; i++) {
-	        String filename_replace = java_version + "/" + i + "-" + filename.replace('#', (char)i);
-	        File f = new File(filename_replace);
-	        try {
-	            f.createNewFile();
-	        } catch (Exception e) {
-	            System.out.println("error: " + i);
-	            e.printStackTrace();
-	        }
-	    }
-	    
         Response<Map<String, Object>> response = new Response<>();
         Map<String, Object> user = new HashMap<>();
         user.put("name", "王耀祖1");
