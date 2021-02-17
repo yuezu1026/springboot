c7be3f86e9ce5d9920b5fa4331e0714bde868d3f
yuezu1026 <yuezu1026@sina.com>
2021-02-17 10:25:48 +0800

    add

---
+++ src/main/java/com/rick/sb/ProductController.java
@@ -0,0 +1,25 @@
+package com.rick.sb;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping(value = "/pro")
+public class ProductController {
+	
+	@GetMapping("/v2/get")
+    public Response<Map<String, Object>> get(){
+        Response<Map<String, Object>> response = new Response<>();
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", "王耀祖");
+        user.put("age", 27);
+        response.setData(user); 
+        return  response;
+    }
+}
