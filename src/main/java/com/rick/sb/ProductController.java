bca3e148377668f91a7905d21d655d562910f8f9
yuezu1026 <yuezu1026@sina.com>
2021-02-27 19:39:10 +0800

    wangyz

--- src/main/java/com/rick/sb/ProductController.java
+++ src/main/java/com/rick/sb/ProductController.java
@@ -18,7 +18,7 @@ public class ProductController {
 	    
         Response<Map<String, Object>> response = new Response<>();
         Map<String, Object> user = new HashMap<>();
-        user.put("name", "王耀祖3");
+        user.put("name", "王耀祖4");
         user.put("age", 22);
         response.setData(user); 
         return  response;
